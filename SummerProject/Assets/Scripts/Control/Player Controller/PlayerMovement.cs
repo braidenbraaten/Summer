c714821e825c7121c6dd3bc60728b11e329ecf9f
Braiden <braidenbraaten@gmail.com>
2016-06-22 08:06:08 -0700

    made ship grey box model, working on player controller

--- SummerProject/Assets/Scripts/Control/Player Controller/PlayerMovement.cs
+++ SummerProject/Assets/Scripts/Control/Player Controller/PlayerMovement.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
     public GameObject playerObject;
+    public Vector2 MouseSpeed;
 	// Use this for initialization
 	void Start () {
 	
@@ -10,14 +11,26 @@ public class PlayerMovement : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        float x = Input.GetAxis("Horizontal") * 0.1f;
-        float z = Input.GetAxis("Vertical") * 0.1f;
+        float WKey = Input.GetAxis("Horizontal") * 0.1f;
+        float SKey = Input.GetAxis("Vertical") * 0.1f;
 
-        
-        float xRotation = Input.mousePosition.normalized.y;
+
+        float xRotation = Input.GetAxis("Mouse X") * 0.1f;
+        float yRotation = Input.GetAxis("Mouse Y") * 0.1f;
         Debug.Log(xRotation);
        
-        playerObject.transform.Translate(x, 0, z);
-        playerObject.transform.Rotate(xRotation, 0, 0);
+        playerObject.transform.Translate(WKey, 0, SKey);
+        if (xRotation > Screen.width / 2)
+        {
+            playerObject.transform.Rotate(0, MouseSpeed.x, 0);
+        }
+
+        if (xRotation < Screen.width / 2)
+        {
+            playerObject.transform.Rotate(0, -MouseSpeed.x, 0);
+        }
+
+
+
     }
 }
